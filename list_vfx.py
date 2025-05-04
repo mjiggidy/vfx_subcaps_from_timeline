c111839d79cfe4a75a756c7525ef231cf8c452db
Michael Jordan <michael@glowingpixel.com>
2025-05-03 19:55:28 -0700

    Plate info

--- list_vfx.py
+++ list_vfx.py
@@ -1,5 +1,5 @@
 import sys, uuid
-import avb, avbutils
+import avb, avbutils, timecode
 
 VFX_IDS_TRACK_LABEL = "VFX IDs"
 """Label given to the track containing  VFX ID subcaps"""
@@ -14,6 +14,16 @@ def get_subcap_caption(track_effect:avb.trackgroups.TrackEffect):
 			user_param = param.value
 			if user_param.uuid == UUID_SUBCAP_TEXT:
 				return user_param.data.decode("utf-8")
+			
+
+def get_plate_at_position(offset_from_start:timecode.Timecode, sequence:avb.components.Sequence) -> tuple[avb.components.Component, int]:
+
+	return sequence.nearest_component_at_time(offset_from_start.frame_number+1)
+
+	masterclip = avbutils.matchback_to_masterclip(plate)
+
+	return masterclip, plate_offset
+	
 
 
 
@@ -45,20 +55,31 @@ if __name__ == "__main__":
 		# Get dailies/plates track (V1)
 		dailies_track = next(avbutils.get_tracks_from_composition(first_timeline, avbutils.TrackTypes.PICTURE, index=1))
 		
-		components = avbutils.get_components_from_track_component(vfx_track.component)
 		
+		# Get VFX IDs and timecodes
 		tc_current = timeline_timecode.start
-		for component in components:
+
+		for component in avbutils.get_components_from_track_component(vfx_track.component):
 			
 			if not isinstance(component, avb.components.Filler):
 
 				if "attributes" not in component.property_data or "_EFFECT_PLUGIN_NAME" not in component.attributes:
 					print("Unknown attributes")
+					
 				if not component.attributes["_EFFECT_PLUGIN_NAME"].lower() == "subcap":
 					print("Not Subclip")
 				
-				subcap_text = get_subcap_caption(component)
+				else:
+					subcap_text = get_subcap_caption(component)
+					
+					plate, plate_offset = get_plate_at_position(tc_current - timeline_timecode.start, dailies_track.component)
+					plate_masterclip = avbutils.matchback_to_masterclip(plate)
+					plate_source = avbutils.matchback_to_sourcemob(plate_masterclip)
+					tc_source = avbutils.get_timecode_range_for_composition(plate_source)
+					tc_plate = timecode.TimecodeRange(start=timeline_timecode.start + plate_offset, duration=plate.length)
 
-				print(f"{tc_current} - {tc_current + component.length}\t{subcap_text}")
+					print(f"{tc_current} - {tc_current + component.length}\t{subcap_text}\t{plate_masterclip.name.ljust(10)}\t{plate_source.name.ljust(10)}\t{tc_plate.start} - {tc_plate.end}\t{tc_source}")
+
+				
 			
 			tc_current += component.length
